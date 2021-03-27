02b1571e134aa1b790cfc3f9b5c3bee03c148956
achmegantara <achmegantara@gmail.com>
2021-03-27 22:25:01 +0700

    testing modules done

--- basic-node/app.js
+++ basic-node/app.js
@@ -2,3 +2,10 @@
 
 // fs.copyFileSync("text.txt", "text3.txt");
 // console.log("text.txt was copied to text2.txt");
+
+const hero = require("superheroes");
+
+for(let i=0; i<10; i++){
+const nameHero = hero.random();
+console.log(nameHero);
+}
