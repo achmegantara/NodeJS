2c034c6857d6d3a34c13626008f9fce41fdd0666
achmegantara <achmegantara@gmail.com>
2021-03-27 22:21:56 +0700

    testing external modules superheroes

--- basic-node/app.js
+++ basic-node/app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.copyFileSync("text.txt", "text3.txt");
-console.log("text.txt was copied to text2.txt");
+// fs.copyFileSync("text.txt", "text3.txt");
+// console.log("text.txt was copied to text2.txt");
