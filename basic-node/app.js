bd2ab163083cdeecfc6898c52bfe5305725467aa
achmegantara <achmegantara@gmail.com>
2021-03-28 21:33:21 +0700

    testing generating modul

--- basic-node/app.js
+++ basic-node/app.js
@@ -3,9 +3,14 @@
 // fs.copyFileSync("text.txt", "text3.txt");
 // console.log("text.txt was copied to text2.txt");
 
-const hero = require("superheroes");
+// const hero = require("superheroes");
 
-for(let i=0; i<10; i++){
-const nameHero = hero.random();
-console.log(nameHero);
-}
+// for(let i=0; i<10; i++){
+// const nameHero = hero.random();
+// console.log(nameHero);
+// }
+
+const op = require("./module");
+
+const moduleTambah = op.tambah(10,5);
+console.log(moduleTambah);
